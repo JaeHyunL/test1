e9c0e1b9252011cf0f5cd46625da73e25b0e77bb
JaeHyunL <swaa23@gmail.com>
2020-11-13 11:07:20 +0900

    깜빡햇네

--- app.py
+++ app.py
@@ -158,10 +158,11 @@ def clear():
 
 
 
-#sched = BackgroundScheduler()
-#sched.start()
-#sched.add_job(ierParser, 'interval', seconds=90, id="_2")
-#sched.add_job(doesParser, 'interval', seconds=90, id="_3")
+sched = BackgroundScheduler()
+sched.start()
+sched.add_job(ierParser, 'interval', seconds=600, id="_1")
+sched.add_job(index2, 'interval', seconds=600, id="_2")
+sched.add_job(doesParser, 'interval', seconds=600, id="_3")
 
 
 app.run(host="0.0.0.0", port=5000)
